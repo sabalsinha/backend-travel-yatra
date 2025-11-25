bb4e31a67f2d3944b23375476daf01ffdb3b66f4
sabal sinha <sabalsinha@sabals-MacBook-Air.local>
2025-11-26 00:04:31 +0530

    cors change

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE,PATCH",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 connection();
 app.get('/', (req, res) => {
